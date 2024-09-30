b5a3e5f46ababa36ba6aa9ae4896b27851f27e45
TheYami <luffy90san@gmail.com>
2024-09-30 14:47:47 +0700

    ex-2

--- ex-2.js
+++ ex-2.js
@@ -9,4 +9,15 @@ let isPlatinum;
 
 let hasPromotion;
 
+lastMonthPaidMoreThan4000 = 4001;
+isWeekday = "Friday";
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+isPlatinum = false;
+
+hasPromotion = (lastMonthPaidMoreThan4000 > 4000 && 
+                (isWeekday == "Monday" || isWeekday == "Tueday" || isWeekday == "Wednesday" || isWeekday == "Thursday" || isWeekday == "Friday") && 
+                !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent) ||
+                isPlatinum;
+
 console.log(hasPromotion);
